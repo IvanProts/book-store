a731b35684ab7e41c918289d119f8a6cda42fe57
Ivan <ivan@Ivans-MacBook-Pro.local>
2023-10-31 14:33:26 +0200

    refactored code

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,15 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './modules/user/users.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://ivanprotss:Qwe123@bookstore.aqtncuj.mongodb.net/book-store-testing?retryWrites=true&w=majority'), UsersModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://ivanprotss:Qwe123@bookstore.aqtncuj.mongodb.net/book-store-testing?retryWrites=true&w=majority',
+    ),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
